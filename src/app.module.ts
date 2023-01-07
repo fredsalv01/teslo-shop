eef6b2330ace355e517906f50b39ba5563b9123f
fredalbert37 <fredalbert37@outlook.com>
2023-01-06 23:23:09 -0500

    mostrar imagenes estaticas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -19,6 +21,9 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
     }),
     ProductsModule,
     CommonModule,
