a018f8095aa644bb414d2a78cdda489af84f4100
fredalbert37 <fredalbert37@outlook.com>
2023-01-07 20:14:52 -0500

    agregando el auth entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { join } from 'path';
     CommonModule,
     SeedModule,
     FilesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('text', { nullable: true })
+  fullname: string;
+
+  @Column('text', { nullable: false })
+  documentType: string;
+
+  @Column('text', { unique: true, nullable: false })
+  documentNumber: string;
+
+  @Column('text', { unique: true, nullable: false })
+  email: string;
+
+  @Column('text', { nullable: false })
+  password: string;
+
+  @Column('bool', { default: true })
+  isActive: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+}
